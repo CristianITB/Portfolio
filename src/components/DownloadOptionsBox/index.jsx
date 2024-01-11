fbb66eb03c3c35127790aaef0d950debbc85b108
Cristian <cr.jimenezgascuena@travelport.com>
2024-01-11 20:55:08 +0100

    finished dropdown curriculums download

--- src/components/DownloadOptionsBox/index.jsx
+++ src/components/DownloadOptionsBox/index.jsx
@@ -1,9 +1,8 @@
-import { DownloadBoxContainer, ButtonContainer, LanguageButton, LanguageFlag, CancelButton } from './style';
+import { DownloadBoxContainer, LanguageButton, LanguageFlag, CancelButton } from './style';
 
 export const DownloadOptionsBox = ({ onClose, onDownload }) => {
     return (
       <DownloadBoxContainer>
-        <ButtonContainer>
           <LanguageButton onClick={() => onDownload('eng')}>
             <LanguageFlag src={process.env.PUBLIC_URL + '/unitedKingdomFlag.png'} alt="United Kingdom's flag" />
             Download CV
@@ -12,7 +11,6 @@ export const DownloadOptionsBox = ({ onClose, onDownload }) => {
             <LanguageFlag src={process.env.PUBLIC_URL + '/spainFlag.png'} alt="Spain's flag" />
             Descargar CV
           </LanguageButton>
-        </ButtonContainer>
       </DownloadBoxContainer>
     );
   };
--- src/components/DownloadOptionsBox/style.js
+++ src/components/DownloadOptionsBox/style.js
@@ -2,36 +2,22 @@ import styled from '@emotion/styled'
 import { withTheme } from '../../styles/styles.helper'
 
 export const DownloadBoxContainer = styled.div`
+  ${withTheme('background-color', 'colors.goldBackground')}
   position: absolute;
   top: 10%;
   right: 2%;
-  ${withTheme('background-color', 'colors.goldBackground')}
   border: 2px solid #BFA181;
   border-radius: 12px;
-  padding: 25px;
+  padding: 16px;
   display: flex;
   flex-direction: column;
-  align-items: center;
-
-  ::before {
-    content: '';
-    position: absolute;
-    top: -15px; /* Adjust this value to control the distance from the button */
-    left: 50%;
-    transform: translateX(-50%);
-    width: 0;
-    height: 0;
-    border-style: solid;
-    border-width: 10px 7px 0;
-    border-color: rgba(191, 161, 129, 0.55) transparent transparent transparent;
-  }
+  gap: 20px;
 `;
 
 export const LanguageButton = styled.button`
-  background-color: transparent;
   ${withTheme('font-weight', 'fontWeight.bold')};
-  display: flex;
-  align-items: center;
+  ${withTheme('font-size', 'sizes.vw07')};
+  background-color: transparent;
   cursor: pointer;
   transition: transform 0.3s ease-in-out;
 
@@ -40,12 +26,8 @@ export const LanguageButton = styled.button`
   }
 `
 
-export const ButtonContainer = styled.div`
-  display: flex;
-  gap: 20px;
-`;
-
 export const LanguageFlag = styled.img`
   height: 15px;
-  margin-right: 5px; /* Adjust this value based on your design preference */
+  margin-right: 5px;
+  vertical-align: top;
 `
